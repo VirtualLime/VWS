923ceda4cf6beef70d76d67335ebd696cf6f1615
Tyler <104088514+VirtualLime@users.noreply.github.com>
2022-11-13 13:27:26 -0400

    Add files via upload

---
+++ reg.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="description" content="Virtual Wellness System"/>
+    <meta name="robots" content="index, follow"/>
+    <link rel="stylesheet" type="text/css" href="main_style.css"/>
+    <title>Virtual Wellness System</title>
+</head>
+
+<body>
+    <div class="entire_page">
+        <div class="reg-header">
+            <h1 id="title">Virtual Wellness System</h1>
+        </div>
+        <div class="main_body">
+            <div class="username_form">
+                <form action="reg.php" method="post" id="username">
+                <label id="username_form_label">Username:</label>
+                <input type="text" name="username_input" placeholder="username"><br>
+            </div>
+            <div class="password_form">
+                <form action="reg.php" method="post" id="password">
+                <label id="password_form_label">Password:</label>
+                <input type="password" name="password_input" placeholder="password"><br>
+            </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
